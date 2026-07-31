eca87829332aa4a101603cf5e968b546d193c60b
ZhongdaMeng <mengzhongda@foxmail.com>
2026-07-31 11:21:21 +0800

    fix：修改模型切换时机

--- src/components/HomeContent/Index.tsx
+++ src/components/HomeContent/Index.tsx
@@ -238,6 +238,7 @@ export const HomeContent = () => {
                     <InputTools
                         canSend={inputInfo.length > 0 && !isStreaming}
                         inputToolsClick={sendMsg}
+                        allowModelSelect={id === 'newchat' && !currentConversationId}
                     />
                 </div>
             </div>
